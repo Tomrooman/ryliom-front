3be8e701191b9c34a9184a84de42432cba61e36e
Tom Rooman <Tomrooman@hotmail.com>
2022-08-29 01:03:04 +0200

    feat: table wit htrades history

--- src/@types/trades.ts
+++ src/@types/trades.ts
@@ -1,4 +1,5 @@
 export type Trades = {
+  [key: string]: boolean | string | number;
   _id: string;
   inPosition: boolean;
   price: number;
--- src/components/TradesHistoryComponent/TradesHistoryComponent.tsx
+++ src/components/TradesHistoryComponent/TradesHistoryComponent.tsx
@@ -15,71 +15,75 @@ import { Trades } from '../../@types/trades';
 type tradesHistoryProps = unknown;
 
 interface Column {
-  id: 'name' | 'code' | 'population' | 'size' | 'density';
+  id: string;
   label: string;
   minWidth?: number;
-  align?: 'right';
-  format?: (value: number) => string;
+  align?: 'right' | 'left' | 'center';
+  format?: (value: any) => string;
 }
 
 const columns: readonly Column[] = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  { id: 'inPosition', label: 'In position', minWidth: 50, align: 'center' },
+  { id: 'price', label: 'Price', minWidth: 50, align: 'center' },
   {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toLocaleString('en-US'),
+    id: 'profit',
+    label: 'Profit',
+    minWidth: 50,
+    align: 'center',
+    // format: (value: number) => value.toLocaleString('en-US'),
   },
   {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toLocaleString('en-US'),
+    id: 'takePrice',
+    label: 'Take price',
+    // minWidth: 170,
+    align: 'center',
+    // format: (value: number) => value.toFixed(2),
   },
   {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toFixed(2),
+    id: 'stopPrice',
+    label: 'Stop price',
+    // minWidth: 170,
+    align: 'center',
+    // format: (value: number) => value.toFixed(2),
+  },
+  {
+    id: 'type',
+    label: 'Type',
+    // minWidth: 170,
+    align: 'center',
+    // format: (value: number) => value.toFixed(2),
+  },
+  {
+    id: 'inAt',
+    label: 'In at',
+    // minWidth: 170,
+    align: 'center',
+    // format: (value: number) => value.toFixed(2),
+  },
+  {
+    id: 'outAt',
+    label: 'Out at',
+    // minWidth: 170,
+    align: 'center',
+    // format: (value: number) => value.toFixed(2),
+  },
+  {
+    id: 'createdAt',
+    label: 'Created at',
+    // minWidth: 170,
+    align: 'center',
+    // format: (value: number) => value.toFixed(2),
+  },
+  {
+    id: 'updatedAt',
+    label: 'Updated at',
+    // minWidth: 170,
+    align: 'center',
+    // format: (value: number) => value.toFixed(2),
   },
 ];
 
-interface Data {
-  name: string;
-  code: string;
-  population: number;
-  size: number;
-  density: number;
-}
-
-function createData(name: string, code: string, population: number, size: number): Data {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
-
-const TradesHistoryComponent: FC<tradesHistoryProps> = (props) => {
+const TradesHistoryComponent: FC<tradesHistoryProps> = () => {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
   const [tradesHistory, setTradesHistory] = useState<Trades[]>([]);
@@ -91,6 +95,7 @@ const TradesHistoryComponent: FC<tradesHistoryProps> = (props) => {
   const getTradesHistory = async () => {
     const { data } = await axios.get('trades/history');
     console.log('data : ', data);
+    setTradesHistory(data);
   };
 
   const handleChangePage = (event: unknown, newPage: number) => {
@@ -116,13 +121,13 @@ const TradesHistoryComponent: FC<tradesHistoryProps> = (props) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
-              <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+            {tradesHistory.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((trade) => (
+              <TableRow hover role="checkbox" tabIndex={-1} key={trade.inAt}>
                 {columns.map((column) => {
-                  const value = row[column.id];
+                  const value = trade[column.id];
                   return (
                     <TableCell key={column.id} align={column.align}>
-                      {column.format && typeof value === 'number' ? column.format(value) : value}
+                      {column.format ? column.format(value) : value}
                     </TableCell>
                   );
                 })}
@@ -134,7 +139,7 @@ const TradesHistoryComponent: FC<tradesHistoryProps> = (props) => {
       <TablePagination
         rowsPerPageOptions={[10, 25, 100]}
         component="div"
-        count={rows.length}
+        count={tradesHistory.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
