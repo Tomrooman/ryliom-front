c0e8aded209caec06add9be0d39f2fb06cac671f
Tom Rooman <Tomrooman@hotmail.com>
2022-10-11 23:22:20 +0200

    wip on statistics

--- src/components/AnalyzeComponent/AnalyzeComponent.tsx
+++ src/components/AnalyzeComponent/AnalyzeComponent.tsx
@@ -6,7 +6,21 @@ import { Trades } from '../../@types/trades';
 import axios from '../../api/axios';
 import TradesAPI from '../../api/tradesApi';
 import TradesListComponent from '../TradesListComponent/TradesListComponent';
+import utils from '../utils/utils';
 import styles from './AnalyzeComponent.module.scss';
+
+type Stats = {
+  totalTrades: number;
+  win: StatsDetails;
+  lose: StatsDetails;
+  profit: number;
+};
+
+type StatsDetails = {
+  count: number;
+  percentage: number;
+  profit: number;
+};
 
 type AnalyzeProps = unknown;
 
@@ -17,6 +31,7 @@ const AnalyzeComponent: FC<AnalyzeProps> = () => {
   const [selectedYear, setSelectedYear] = useState<string>('all');
   const [selectedMonth, setSelectedMonth] = useState<string>('all');
   const [selectedDay, setSelectedDay] = useState<string>('all');
+  const [stats, setStats] = useState<Stats>();
 
   useEffect(() => {
     getTradesHistory();
@@ -32,6 +47,27 @@ const AnalyzeComponent: FC<AnalyzeProps> = () => {
     const { data } = await axios.get('pages/infos');
     setPagesInfos(data);
   };
+
+  useEffect(() => {
+    const winTrades = filteredTrades.filter((trade) => trade.profit > 0);
+    const loseTrades = filteredTrades.filter((trade) => trade.profit <= 0);
+    const winProfit = utils.formatNumber(winTrades.reduce((total, trade) => trade.profit + total, 0));
+    const loseProfit = utils.formatNumber(loseTrades.reduce((total, trade) => trade.profit + total, 0));
+    setStats({
+      totalTrades: filteredTrades.length,
+      win: {
+        count: winTrades.length,
+        percentage: utils.formatNumber((100 * winTrades.length) / filteredTrades.length),
+        profit: winProfit,
+      },
+      lose: {
+        count: loseTrades.length,
+        percentage: utils.formatNumber((100 * loseTrades.length) / filteredTrades.length),
+        profit: loseProfit,
+      },
+      profit: utils.formatNumber(winProfit + loseProfit),
+    });
+  }, [filteredTrades]);
 
   const applyDateFilter = () => {
     const filtered = tradesHistory.filter((trade) => {
@@ -125,11 +161,38 @@ const AnalyzeComponent: FC<AnalyzeProps> = () => {
 
   return (
     <div className={styles.analyzeContainer}>
-      <div className={styles.analyzeDateSelection}>
-        {renderChoices('Year', selectedYear, getYearChoicesFromDate, setSelectedYear)}
-        {renderChoices('Month', selectedMonth, getMonthChoicesFromDateByYear, setSelectedMonth)}
-        {renderChoices('Day', selectedDay, getDayChoicesFromDateByMonth, setSelectedDay)}
-        <Button onClick={applyDateFilter}>Confirmer</Button>
+      <div className={styles.analyzeDateContainer}>
+        <h2>Analyze</h2>
+        <div className={styles.analyzeDateSelection}>
+          {renderChoices('Year', selectedYear, getYearChoicesFromDate, setSelectedYear)}
+          {renderChoices('Month', selectedMonth, getMonthChoicesFromDateByYear, setSelectedMonth)}
+          {renderChoices('Day', selectedDay, getDayChoicesFromDateByMonth, setSelectedDay)}
+          <Button variant="outlined" onClick={applyDateFilter}>
+            Confirmer
+          </Button>
+        </div>
+      </div>
+      <div className={styles.statisticsContainer}>
+        {stats?.totalTrades ? (
+          <>
+            <div className={styles.statistics}>
+              <div className={styles.statisticsLeft}>
+                <p>Trade(s) gagnant(s): {stats.win.count}</p>
+                <p>Percentage: {stats.win.percentage}</p>
+                <p>Profit: {stats.win.profit}</p>
+              </div>
+              <div className={styles.statisticsRight}>
+                <p>Trade(s) perdant(s): {stats.lose.count}</p>
+                <p>Percentage: {stats.lose.percentage}</p>
+                <p>Perte: {stats.lose.profit}</p>
+              </div>
+            </div>
+            <div className={styles.statisticsTotal}>
+              <p>Trades: {stats.totalTrades}</p>
+              <p>Profit: {stats.profit}</p>
+            </div>
+          </>
+        ) : null}
       </div>
       <div className={styles.tradesContainer}>
         <TradesListComponent tradesHistory={filteredTrades} />
---
+++ src/components/utils/utils.tsx
@@ -0,0 +1,5 @@
+const utils = {
+  formatNumber: (data: number) => Math.round(data * 100) / 100,
+};
+
+export default utils;
